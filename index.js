41be5ced2768c64ef29690661a6cade743c6ce70
barry-kn <carrybarryklein49@gmail.com>
2023-03-14 16:31:05 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a){
+let result;
+if (a <=400){
+return result='This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (a <=2499){
+  return result='That will be 30 dollars!'
 }
+else (a>=2500);{
+ return result='No can do.'
+}
+}
+scuberGreetingForFeet(300)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function ternaryCheckCity(b){
+let result;
+if(b==='NYC'){
+  return result='Ok, sounds good.'
 }
+else{
+  return result='No go.'
+}
+}
+ternaryCheckCity("nairobi")
+
+
+function switchOnCharmFromTip(c){
+if (c==="generous"){
+  return result="Thank you so much."
+}
+else if( c==='not as generous'){
+  return result="Thank you ."
+}
+else{
+  return result="bye."
+}
+}
+switchOnCharmFromTip()
